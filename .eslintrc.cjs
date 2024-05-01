7b850ee8b1dea8d8f050a22e3be33a8dd6728220
Dinh-Van Colomban <van.colomban@gmail.com>
2024-05-01 13:38:36 +0200

    fix(lint): eslint config and dependencies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,17 @@
 module.exports = {
   root: true,
   plugins: ['@dvcol/presets'],
-  extends: [
-      'plugin:@dvcol/presets/typescript', 'plugin:@dvcol/presets/vitest'],
-}
+  extends: ['plugin:@dvcol/presets/base', 'plugin:@dvcol/presets/prettier'],
+  overrides: [
+    {
+      files: ['*.spec.*', '*.test.*'],
+      plugins: ['@dvcol/presets'],
+      extends: ['plugin:@dvcol/presets/vitest', 'plugin:@dvcol/presets/prettier'],
+    },
+    {
+      files: ['*.ts', '*.cts', '*.mts', '*.tsx'],
+      plugins: ['@dvcol/presets'],
+      extends: ['plugin:@dvcol/presets/typescript', 'plugin:@dvcol/presets/prettier'],
+    },
+  ],
+};
